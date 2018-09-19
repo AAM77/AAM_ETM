76e0896b68eebc319917fd5e52371117a9baa348
AAM77 <Adeel.A.Mhd@gmail.com>
2018-09-18 22:07:44 -0400

    Event table lists participants for a task correctly

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,14 @@ class Task < ApplicationRecord
   belongs_to :event
   has_many :user_tasks
   has_many :users, through: :user_tasks
+
+  def list_participants
+    list = self.users.map { |u| u.username.capitalize }
+    if list.size == 1
+      list.first
+    else
+      list.join(", ")
+    end
+  end
+
 end
