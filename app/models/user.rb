0f9e60a53ebfda8d21c16c636f2c4bae01dd86e9
AAM77 <Adeel.A.Mhd@gmail.com>
2018-11-07 03:36:27 -0500

    Moved uniqueness validations for :email and :username further up, so that they are checked first after their presence has been verified.

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,12 +14,12 @@ class User < ApplicationRecord
   validates :username, presence: true, presence: { message: "The username cannot be blank." }, on: :update
   validates :email, presence: true, on: :create, presence: { message: "Email cannot be blank.", unless: :other_provider? }
   validates :password, presence: true, presence: { message: "Password cannot be blank." }, on: :create, unless: :other_provider?
+  validates_uniqueness_of :username, case_sensitive: false, message: "That username is invalid."
+  validates_uniqueness_of :email, case_sensitive: false, message: "That email is invalid.", on: :create
   validates :username, length: { in: 5..20, message: "The length of the username must be between 5 and 20 characters long." }
   validates :password, length: { in: 5..30, message: "The length of the password must be between 5 and 30 characters long." }, unless: :other_provider?
   validates :username, format: { without: /[\?\<\>\'\,\?\[\]\}\{\=\-\)\(\*\&\^\%\$\#\`\~\{\}\@]/, message: "The username can contain only letters, digits, periods, and underscores" }, unless: :other_provider?
   validates :email, format: { without: /[\?\<\>\'\,\?\[\]\}\{\=\-\)\(\*\&\^\%\$\#\`\~\{\}]/, message: "The email can contain only letters, digits, periods, underscores, & the @ symbol." }, unless: :other_provider?
-  validates_uniqueness_of :username, case_sensitive: false, message: "That username is invalid."
-  validates_uniqueness_of :email, case_sensitive: false, message: "That email is invalid.", on: :create
 
   after_create :assign_uniq_username
   after_create :capitalize_first_and_last_name
