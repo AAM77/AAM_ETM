0489a235491a9c3278812471b4ff5669aa6c25cf
AAM77 <Adeel.A.Mhd@gmail.com>
2019-03-28 00:45:56 -0400

    added ajax to user_index.js

--- app/assets/javascripts/user_index.js
+++ app/assets/javascripts/user_index.js
@@ -1,16 +1,23 @@
 
 function display_users() {
-  $('#users').append(
-    `
-    <ol>
-      <li>
-        "USER 1"
-      </li>
-    </ol>
-
-    `
-    );
+  $.ajax({
+    url: `/users.json`,
+    method: 'GET'
+  })
+  .done(function(users) {
+    $('#users').append('<ol></ol>')
+    users.forEach( user => {
+      $('#users ol').append(
+        `
+        <li>
+          <a href="/users/${user['id']}" target="_blank">${user['username']}</a>
+        </li>
+        `
+      );
+    })
+  })
 }
+
 
 // DOCUMENT.READY FUNCTION
 $(function() {
