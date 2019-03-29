fa6ef6f6d2b2eb91e347c5a5585f3e83a3a74480
github username <github email address>
2019-03-29 19:20:57 -0400

    Used jquery to generate content for the user adminned events table on the user's show page.

--- app/assets/javascripts/users_show.js
+++ app/assets/javascripts/users_show.js
@@ -11,11 +11,36 @@ class User {
   }
 }
 
+class UserEvent {
+  constructor(object) {
+    this.id = object.id
+    this.name = object.name
+  }
+}
+
+UserEvent.prototype.listItemLink = function() {
+  return (
+    `
+    <li class="list-group-item">
+      <a href="/events/${this.id}" target="_blank">${this.name}</a>
+    </li>
+    `
+  )
+}
+
 function pageTitle() {
   $.get(`${window.location.href}.json`, function(data) {
     const user = new User(data)
     $('#user-homepage-header').append(`${user.username}'s Homepage`)
     $('#users-total-points').append(`${user.username}'s Points: ${user.total_points}`)
+    $('#adminned-events-title').append(`Events ${user.username} Created`)
+    $('#adminned-events-list').empty()
+
+    user.adminned_events.forEach( event => {
+      let newEvent = new UserEvent(event)
+      let eventHTML = newEvent.listItemLink()
+      $('#adminned-events-list').append(eventHTML)
+    })
   })
 }
 
