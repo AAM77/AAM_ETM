f3b9e1c9e43d066d59626637eec73b8c292e1dd3
github username <Adeel.A.Mhd@gmail.com>
2019-03-30 21:15:37 -0400

    Removed the all_friendships method. Modified the all_friends method to include the friendship id

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :crnt_user, :id, :first_name, :last_name, :telephone_num,
              :address, :email, :username, :total_points,
-             :all_friends, :all_friendships, :solo_tasks, :group_tasks
+             :all_friends, :solo_tasks, :group_tasks
 
   has_many :events, through: :user_events, dependent: :destroy
   # has_many :friendships, dependent: :destroy
@@ -15,24 +15,27 @@ class UserSerializer < ActiveModel::Serializer
 
   def all_friends
     (object.friends + object.inverse_friends).map do |friend|
+      sent_friendships = friend.friendships.map { |friendship| { id: friendship.id } if friendship.user_id == current_user.id || friendship.friend_id == current_user.id }.compact
+      received_friendships = friend.inverse_friendships.map { |friendship| { id: friendship.id } if friendship.user_id == current_user.id || friendship.friend_id == current_user.id }.compact
+      total_friendships = sent_friendships + received_friendships
       {
         id: friend.id,
         username: friend.username,
         events: friend.events.map { |event| { id: event.id, name: event.name, admin_id: event.admin_id } },
-        friendships: friend.friendships.map { |friendship| { id: friendship.id, user_id: friendship.user_id, friend_id: friendship.friend_id } if friendship.user_id == current_user.id || friendship.friend_id == current_user.id }.compact
+        friendship_id: total_friendships[0]
       }
     end
   end
-
-  def all_friendships
-    (object.friendships + object.inverse_friendships).map do |friendship|
-      {
-        id: friendship.id,
-        user_id: friendship.user_id,
-        friend_id: friendship.friend_id
-      }
-    end
-  end
+  #
+  # def all_friendships
+  #   (object.friendships + object.inverse_friendships).map do |friendship|
+  #     {
+  #       id: friendship.id,
+  #       user_id: friendship.user_id,
+  #       friend_id: friendship.friend_id
+  #     }
+  #   end
+  # end
 
   def solo_tasks
     object.tasks.map { |task| {
